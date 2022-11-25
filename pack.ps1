3827326c594e9c0a2d0bf30c1587d1e08a527432
Valerius Weigandt <v.weigandt@orcadent.de>
2022-11-25 12:25:35 +0100

    offline build script

---
+++ pack.ps1
@@ -0,0 +1,31 @@
+$GitVersion_NuGetVersion = "2.22.2"
+
+$pattern1 = '\[assembly: AssemblyVersion\("(.*)"\)\]'
+$pattern2 = '\[assembly: AssemblyFileVersion\("(.*)"\)\]'
+
+#AssemblyFileVersion
+
+Get-ChildItem AssemblyInfo.cs -Recurse | % { 
+
+        (Get-Content $_.FullName)| %{ 
+                    $_ -replace $pattern1, ('[assembly: AssemblyVersion("{0}")]' -f $GitVersion_NuGetVersion) -replace $pattern2, ('[assembly: AssemblyFileVersion("{0}")]' -f $GitVersion_NuGetVersion)                       
+        } | Set-Content $_ }
+
+
+#if($_ -match $pattern1){
+#    #$fileVersion = [version]$matches[1]
+#    #$newVersion = "{0}.{1}.{2}.{3}" -f $fileVersion.Major, $fileVersion.Minor, $fileVersion.Build, ($fileVersion.Revision + 1)
+#    '[assembly: AssemblyVersion({0})]' -f $GitVersion_NuGetVersion
+# } else {
+#    # Output line as is
+#    $_
+# }    
+        
+
+##DevShell:
+msbuild Source\HelixToolkit.AppVeyor.sln /property:Platform="Any CPU" /property:Version=$GitVersion_NuGetVersion /verbosity:minimal /p:SourceLinkCreate=true /m /property:Configuration=Release
+
+#Create Nuget Packages
+Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "nuget.exe"
+
+Get-ChildItem Source\*.nuspec | % {.\nuget.exe pack $_ -version $GitVersion_NuGetVersion -Symbols -SymbolPackageFormat snupkg}
